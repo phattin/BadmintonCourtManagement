5e10d9a894b609091f55d9a12c17a0dfa88862fa
Santadura <thanhluan200510a@gmail.com>
2025-09-22 22:19:31 +0700

    login

--- BadmintonCourtManagement/GUI/LoginGUI.Designer.cs
+++ BadmintonCourtManagement/GUI/LoginGUI.Designer.cs
@@ -28,18 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            textBox1 = new TextBox();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            label3 = new Label();
+            pictureBox3 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            textBox2 = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(235, 235, 235);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(418, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(567, 662);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Location = new Point(0, 195);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(567, 51);
+            panel2.TabIndex = 8;
+            panel2.Paint += panel2_Paint;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.Black;
+            textBox1.Location = new Point(67, 14);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Username";
+            textBox1.Size = new Size(487, 24);
+            textBox1.TabIndex = 3;
+            textBox1.Tag = "";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.White;
+            pictureBox2.Image = Properties.Resources.user_solid_full;
+            pictureBox2.Location = new Point(12, 6);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(39, 42);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(125, 179, 125);
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.Cursor = Cursors.AppStarting;
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(197, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 55);
+            button1.TabIndex = 7;
+            button1.Text = "Đăng nhập";
+            button1.TextImageRelation = TextImageRelation.TextAboveImage;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(125, 179, 125);
+            label3.Location = new Point(212, 117);
+            label3.Name = "label3";
+            label3.Size = new Size(151, 37);
+            label3.TabIndex = 2;
+            label3.Text = "Đăng nhập";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            label3.Click += label3_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.White;
+            pictureBox3.Image = Properties.Resources.lock_solid_full;
+            pictureBox3.Location = new Point(12, 3);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(39, 42);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(70, 116);
+            label1.Name = "label1";
+            label1.Size = new Size(193, 37);
+            label1.TabIndex = 1;
+            label1.Text = "WELCOME TO ";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(70, 153);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 37);
+            label2.TabIndex = 2;
+            label2.Text = "SCLL ";
+            label2.Click += label2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.image_3;
+            pictureBox1.Location = new Point(70, 266);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(253, 264);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(pictureBox3);
+            panel3.Location = new Point(418, 266);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(567, 51);
+            panel3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.ForeColor = Color.Black;
+            textBox2.Location = new Point(67, 14);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Password";
+            textBox2.Size = new Size(487, 24);
+            textBox2.TabIndex = 3;
+            textBox2.Tag = "";
+            textBox2.UseSystemPasswordChar = true;
             // 
             // LoginGUI
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(978, 644);
+            BackColor = Color.FromArgb(125, 179, 125);
+            ClientSize = new Size(984, 661);
+            Controls.Add(panel3);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Margin = new Padding(2);
             Name = "LoginGUI";
             Text = "Đăng nhập";
+            Load += LoginGUI_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private TextBox textBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Button button1;
+        private Panel panel2;
+        private Panel panel3;
+        private TextBox textBox2;
     }
 }
