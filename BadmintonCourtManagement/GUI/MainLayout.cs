8f686fd77aea0756d8f271693682eb814619c01b
tin220905 <tin220905@gmail.com>
2025-09-23 12:18:14 +0700

    login

---
+++ BadmintonCourtManagement/GUI/MainLayout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BadmintonCourtManagement.BUS;
+using BadmintonCourtManagement.DTO;
+
+namespace BadmintonCourtManagement.GUI
+{
+    public partial class MainLayout : Form
+    {
+        public MainLayout(AccountDTO account)
+        {
+            AccountDTO accountCurrent = account;
+            InitializeComponent();
+        }
+    }
+}
