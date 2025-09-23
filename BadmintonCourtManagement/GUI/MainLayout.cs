9524bf63f1f523f4dea640b024a513caee628849
unknown <tungthien408@gmail.com>
2025-09-23 15:32:28 +0700

    MenuBar Draft #1

--- BadmintonCourtManagement/GUI/MainLayout.cs
+++ BadmintonCourtManagement/GUI/MainLayout.cs
@@ -14,12 +14,76 @@ namespace BadmintonCourtManagement.GUI
 {
     public partial class MainLayout : Form
     {
+        private AccountDTO currentAccount;
+        private UserControl currentPanel;
+        private MenuManager menuManager;
         public MainLayout(AccountDTO account)
         {
             AccountDTO accountCurrent = account;
             InitializeComponent();
+            CreateMenuButtons();
         }
+
+        private void CreateMenuButtons()
+        {
+            menuManager = new MenuManager();
+            var menuItems = new Dictionary<string, EventHandler>
+            {
+                { "Đặt sân", BookCourt_Click },
+                { "Quản lý sân", ManageCourts_Click },
+                { "Khách hàng", Customers_Click },
+                { "Báo cáo", Reports_Click },
+                { "Cài đặt", Settings_Click }
+            };
+
+            menuManager.CreateMenuButtons(menuPanel, menuItems);
+        }
+
+        private void OpenChildPanel(UserControl childPanel)
+        {
+            // Remove current panel if exists
+            if (currentPanel != null)
+            {
+                this.Controls.Remove(currentPanel);
+                currentPanel.Dispose();
+            }
+            
+            // Add new panel
+            currentPanel = childPanel;
+            childPanel.Dock = DockStyle.Fill;
+            this.Controls.Add(childPanel);
+            childPanel.BringToFront();
+            
+            // Keep menu panel on top
+            menuPanel.BringToFront();
+        }
+
+        // Event handlers for menu buttons
+        private void BookCourt_Click(object sender, EventArgs e)
+        {
+            OpenChildPanel(new BookCourtGUI(currentAccount));
+            MessageBox.Show("Đặt sân clicked!");
+        }
+
+        private void ManageCourts_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Quản lý sân clicked!");
+        }
+
+        private void Customers_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Khách hàng clicked!");
+        }
+
+        private void Reports_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Báo cáo clicked!");
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cài đặt clicked!");
+        }
+
     }
-
-
 }
