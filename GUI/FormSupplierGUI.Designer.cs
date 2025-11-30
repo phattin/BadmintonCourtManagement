4b733015759c1729d1110f51acd7c27ff8788418
Thai Tu <dangthaitu2005@gmail.com>
2025-11-30 15:46:09 +0700

    fix

--- GUI/FormSupplierGUI.Designer.cs
+++ GUI/FormSupplierGUI.Designer.cs
@@ -1,3 +1,4 @@
+// FormSupplierGUI.Designer.cs
 namespace BadmintonCourtManagement.GUI
 {
     partial class FormSupplierGUI
@@ -15,13 +16,13 @@ namespace BadmintonCourtManagement.GUI
         {
             tableLayoutPanel1 = new TableLayoutPanel();
             lblTitle = new Label();
-            lblSupplierId = new Label();
+            label1 = new Label();
             txtSupplierId = new TextBox();
-            lblSupplierName = new Label();
+            label2 = new Label();
             txtSupplierName = new TextBox();
-            lblAddress = new Label();
+            label3 = new Label();
             txtAddress = new TextBox();
-            lblEmail = new Label();
+            label4 = new Label();
             txtEmail = new TextBox();
             btnSave = new Button();
             btnCancel = new Button();
@@ -30,125 +31,140 @@ namespace BadmintonCourtManagement.GUI
             // 
             // tableLayoutPanel1
             // 
+            tableLayoutPanel1.BackColor = Color.White;
             tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 215F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Controls.Add(lblSupplierId, 0, 1);
-            tableLayoutPanel1.Controls.Add(txtSupplierId, 1, 1);
-            tableLayoutPanel1.Controls.Add(lblSupplierName, 0, 2);
-            tableLayoutPanel1.Controls.Add(txtSupplierName, 1, 2);
-            tableLayoutPanel1.Controls.Add(lblAddress, 0, 3);
-            tableLayoutPanel1.Controls.Add(txtAddress, 1, 3);
-            tableLayoutPanel1.Controls.Add(lblEmail, 0, 4);
-            tableLayoutPanel1.Controls.Add(txtEmail, 1, 4);
-            tableLayoutPanel1.Controls.Add(btnSave, 0, 5);
-            tableLayoutPanel1.Controls.Add(btnCancel, 1, 5);
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.Controls.Add(lblTitle, 0, 0);
+            tableLayoutPanel1.Controls.Add(label1, 0, 1);
+            tableLayoutPanel1.Controls.Add(txtSupplierId, 1, 1);
+            tableLayoutPanel1.Controls.Add(label2, 0, 2);
+            tableLayoutPanel1.Controls.Add(txtSupplierName, 1, 2);
+            tableLayoutPanel1.Controls.Add(label3, 0, 3);
+            tableLayoutPanel1.Controls.Add(txtAddress, 1, 3);
+            tableLayoutPanel1.Controls.Add(label4, 0, 4);
+            tableLayoutPanel1.Controls.Add(txtEmail, 1, 4);
+            tableLayoutPanel1.Controls.Add(btnSave, 0, 6);
+            tableLayoutPanel1.Controls.Add(btnCancel, 1, 6);
             tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Location = new Point(10, 10);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.Padding = new Padding(12);
-            tableLayoutPanel1.RowCount = 6;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 48F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 48F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 53F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 63F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 59F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Size = new Size(418, 350);
+            tableLayoutPanel1.Padding = new Padding(30, 20, 30, 30);
+            tableLayoutPanel1.RowCount = 7;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 90F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 80F));
+            tableLayoutPanel1.Size = new Size(508, 434);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // lblTitle
             // 
             lblTitle.AutoSize = true;
             tableLayoutPanel1.SetColumnSpan(lblTitle, 2);
-            lblTitle.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
-            lblTitle.Location = new Point(15, 12);
+            lblTitle.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            lblTitle.ForeColor = Color.FromArgb(30, 30, 30);
+            lblTitle.Location = new Point(30, 20);
+            lblTitle.Margin = new Padding(0);
             lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(158, 38);
+            lblTitle.Size = new Size(366, 45);
             lblTitle.TabIndex = 0;
-            lblTitle.Text = "CẬP NHẬT";
-            lblTitle.TextAlign = ContentAlignment.MiddleCenter;
+            lblTitle.Text = "THÊM NHÀ CUNG CẤP";
+            lblTitle.TextAlign = ContentAlignment.TopCenter;
             // 
-            // lblSupplierId
+            // label1
             // 
-            lblSupplierId.Location = new Point(15, 60);
-            lblSupplierId.Name = "lblSupplierId";
-            lblSupplierId.Size = new Size(100, 23);
-            lblSupplierId.TabIndex = 1;
-            lblSupplierId.Text = "Mã nhà cung cấp";
-            lblSupplierId.TextAlign = ContentAlignment.MiddleRight;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label1.Location = new Point(33, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(174, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Mã nhà cung cấp";
+            label1.TextAlign = ContentAlignment.MiddleRight;
             // 
             // txtSupplierId
             // 
             txtSupplierId.BackColor = Color.FromArgb(245, 245, 245);
             txtSupplierId.Enabled = false;
-            txtSupplierId.Location = new Point(230, 63);
+            txtSupplierId.Font = new Font("Segoe UI", 11F);
+            txtSupplierId.Location = new Point(257, 113);
             txtSupplierId.Name = "txtSupplierId";
-            txtSupplierId.Size = new Size(157, 31);
+            txtSupplierId.Size = new Size(218, 37);
             txtSupplierId.TabIndex = 2;
             // 
-            // lblSupplierName
+            // label2
             // 
-            lblSupplierName.Location = new Point(15, 108);
-            lblSupplierName.Name = "lblSupplierName";
-            lblSupplierName.Size = new Size(100, 23);
-            lblSupplierName.TabIndex = 3;
-            lblSupplierName.Text = "Tên nhà cung cấp";
-            lblSupplierName.TextAlign = ContentAlignment.MiddleRight;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label2.Location = new Point(33, 170);
+            label2.Name = "label2";
+            label2.Size = new Size(177, 28);
+            label2.TabIndex = 3;
+            label2.Text = "Tên nhà cung cấp";
+            label2.TextAlign = ContentAlignment.MiddleRight;
             // 
             // txtSupplierName
             // 
-            txtSupplierName.Font = new Font("Segoe UI", 10F);
-            txtSupplierName.Location = new Point(230, 111);
+            txtSupplierName.Font = new Font("Segoe UI", 11F);
+            txtSupplierName.Location = new Point(257, 173);
             txtSupplierName.Name = "txtSupplierName";
-            txtSupplierName.Size = new Size(157, 34);
+            txtSupplierName.Size = new Size(218, 37);
             txtSupplierName.TabIndex = 4;
             // 
-            // lblAddress
+            // label3
             // 
-            lblAddress.Location = new Point(15, 161);
-            lblAddress.Name = "lblAddress";
-            lblAddress.Size = new Size(100, 23);
-            lblAddress.TabIndex = 5;
-            lblAddress.Text = "Địa chỉ";
-            lblAddress.TextAlign = ContentAlignment.MiddleRight;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label3.Location = new Point(33, 230);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 28);
+            label3.TabIndex = 5;
+            label3.Text = "Địa chỉ";
+            label3.TextAlign = ContentAlignment.MiddleRight;
             // 
             // txtAddress
             // 
-            txtAddress.Font = new Font("Segoe UI", 10F);
-            txtAddress.Location = new Point(230, 164);
-            txtAddress.Multiline = true;
+            txtAddress.Font = new Font("Segoe UI", 11F);
+            txtAddress.Location = new Point(257, 233);
             txtAddress.Name = "txtAddress";
-            txtAddress.Size = new Size(157, 31);
+            txtAddress.Size = new Size(218, 37);
             txtAddress.TabIndex = 6;
             // 
-            // lblEmail
+            // label4
             // 
-            lblEmail.Location = new Point(15, 216);
-            lblEmail.Name = "lblEmail";
-            lblEmail.Size = new Size(100, 23);
-            lblEmail.TabIndex = 7;
-            lblEmail.Text = "Email";
-            lblEmail.TextAlign = ContentAlignment.MiddleRight;
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label4.Location = new Point(33, 290);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 28);
+            label4.TabIndex = 7;
+            label4.Text = "Email";
+            label4.TextAlign = ContentAlignment.MiddleRight;
             // 
             // txtEmail
             // 
-            txtEmail.Font = new Font("Segoe UI", 10F);
-            txtEmail.Location = new Point(230, 219);
+            txtEmail.Font = new Font("Segoe UI", 11F);
+            txtEmail.Location = new Point(257, 293);
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(157, 34);
+            txtEmail.Size = new Size(218, 37);
             txtEmail.TabIndex = 8;
             // 
             // btnSave
             // 
+            btnSave.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             btnSave.BackColor = Color.FromArgb(0, 142, 123);
+            btnSave.Cursor = Cursors.Hand;
+            btnSave.FlatAppearance.BorderSize = 0;
             btnSave.FlatStyle = FlatStyle.Flat;
+            btnSave.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             btnSave.ForeColor = Color.White;
-            btnSave.Location = new Point(15, 282);
+            btnSave.Location = new Point(33, 383);
             btnSave.Name = "btnSave";
-            btnSave.Size = new Size(75, 41);
+            btnSave.Size = new Size(218, 39);
             btnSave.TabIndex = 9;
             btnSave.Text = "Lưu";
             btnSave.UseVisualStyleBackColor = false;
@@ -156,12 +172,16 @@ namespace BadmintonCourtManagement.GUI
             // 
             // btnCancel
             // 
+            btnCancel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             btnCancel.BackColor = Color.FromArgb(220, 53, 69);
+            btnCancel.Cursor = Cursors.Hand;
+            btnCancel.FlatAppearance.BorderSize = 0;
             btnCancel.FlatStyle = FlatStyle.Flat;
+            btnCancel.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             btnCancel.ForeColor = Color.White;
-            btnCancel.Location = new Point(230, 282);
+            btnCancel.Location = new Point(257, 383);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(75, 41);
+            btnCancel.Size = new Size(218, 39);
             btnCancel.TabIndex = 10;
             btnCancel.Text = "Hủy";
             btnCancel.UseVisualStyleBackColor = false;
@@ -169,16 +189,15 @@ namespace BadmintonCourtManagement.GUI
             // 
             // FormSupplierGUI
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(11F, 28F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(418, 350);
+            BackColor = Color.White;
+            ClientSize = new Size(528, 454);
             Controls.Add(tableLayoutPanel1);
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            MaximizeBox = false;
-            MinimizeBox = false;
+            Font = new Font("Segoe UI", 10F);
+            MinimumSize = new Size(550, 510);
             Name = "FormSupplierGUI";
-            StartPosition = FormStartPosition.CenterParent;
-            Text = "Nhà cung cấp";
+            Padding = new Padding(10);
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
@@ -186,13 +205,13 @@ namespace BadmintonCourtManagement.GUI
 
         private TableLayoutPanel tableLayoutPanel1;
         private Label lblTitle;
-        private Label lblSupplierId;
+        private Label label1;
         private TextBox txtSupplierId;
-        private Label lblSupplierName;
+        private Label label2;
         private TextBox txtSupplierName;
-        private Label lblAddress;
+        private Label label3;
         private TextBox txtAddress;
-        private Label lblEmail;
+        private Label label4;
         private TextBox txtEmail;
         private Button btnSave;
         private Button btnCancel;
